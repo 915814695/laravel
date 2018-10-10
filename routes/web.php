5411e3fb845742e1bbf23bb68004613cee959837
孙正阳 <sunzhengyang@qdweikong.com>
2018-10-10 10:19:17 +0800

    初始化

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', 'PagesController@root')->name('root');
+
+Auth::routes();
+
+Route::group(['middleware' => 'auth'], function() {
+    Route::get('/email_verify_notice', 'PagesController@emailVerifyNotice')->name('email_verify_notice');
+    Route::get('/email_verification/verify', 'EmailVerificationController@verify')->name('email_verification.verify');
+    Route::get('/email_verification/send', 'EmailVerificationController@send')->name('email_verification.send');
+
+
+//    Route::group(['middleware' => 'email_verified'], function() {
+//        Route::get('/test', function() {
+//            return 'Your email is verified';
+//        });
+//    });
+});
